644eb87eede3354d10038a80838a8d1208cd92fd
timothyjhull <timothyjhull@gmail.com>
2017-03-03 10:29:59 -0500

    Initial Commit

---
+++ SocialProject_Native/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialProject_Native
+//
+//  Created by Timothy Hull on 3/3/17.
+//  Copyright © 2017 Sponti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
